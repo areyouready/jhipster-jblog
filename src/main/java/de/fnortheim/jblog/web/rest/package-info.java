99e349f42cbdd02d2e1a57d33f90f7070f730719
Sebastian Basner <sebastian@are-you-ready.de>
2018-03-18 18:46:44 +0100

    Initial application generated by JHipster-4.14.1

---
+++ src/main/java/de/fnortheim/jblog/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package de.fnortheim.jblog.web.rest;
---
+++ src/main/webapp/app/shared/shared-common.module.ts
@@ -0,0 +1,42 @@
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import locale from '@angular/common/locales/de';
+
+import {
+    JBlogSharedLibsModule,
+    JhiLanguageHelper,
+    FindLanguageFromKeyPipe,
+    JhiAlertComponent,
+    JhiAlertErrorComponent
+} from './';
+
+@NgModule({
+    imports: [
+        JBlogSharedLibsModule
+    ],
+    declarations: [
+        FindLanguageFromKeyPipe,
+        JhiAlertComponent,
+        JhiAlertErrorComponent
+    ],
+    providers: [
+        JhiLanguageHelper,
+        Title,
+        {
+            provide: LOCALE_ID,
+            useValue: 'de'
+        },
+    ],
+    exports: [
+        JBlogSharedLibsModule,
+        FindLanguageFromKeyPipe,
+        JhiAlertComponent,
+        JhiAlertErrorComponent
+    ]
+})
+export class JBlogSharedCommonModule {
+    constructor() {
+        registerLocaleData(locale);
+    }
+}
